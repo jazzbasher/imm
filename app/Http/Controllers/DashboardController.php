df70c71c4b89e239a4aeda9ac62787c9ee943d78
Michael Bartlett <michaelbartlett@icloud.com>
2026-08-26 09:36:08 -0500

    testing

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -62,7 +62,7 @@ class DashboardController extends Controller
                 $query->where('lunch_code', '!=', 3);})->with('user')->get();
 
         // 490 = 8 hours 10 minutes for users who do clock for lunch
-        $otclock = TimeClock::selectRaw('user_id, COUNT(*) as cnt')->whereRaw('TIMESTAMPDIFF(MINUTE, clock_in, clock_out) > ?', [490])->groupBy('user_id')->having('cnt', '>', 0)->where('clock_in', '>=', Carbon::now()->subDays(7)->startOfDay())->whereHas('user', function ($query) {
+        $otclock = TimeClock::selectRaw('user_id, COUNT(*) as cnt')->whereRaw('TIMESTAMPDIFF(MINUTE, clock_in, clock_out) > ?', [540])->groupBy('user_id')->having('cnt', '>', 0)->where('clock_in', '>=', Carbon::now()->subDays(7)->startOfDay())->whereHas('user', function ($query) {
                 $query->where('lunch_code', '=', 3);})->with('user')->get();
 
     
